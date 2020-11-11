3fa60bcac0871df71b1e2347ee3513754d68cb51
Niklas Stambor <ni.stambor@gmail.com>
2020-11-11 20:35:09 +0100

    Did some improvements to the downloads, should download a lot fast now. Python script is a lot cleaner too now, at least if you ask me

--- README.md
+++ README.md
@@ -5,13 +5,14 @@ A download client for TomTom firmware using python3 and various sources
 - python3
 - bs4 (from python's pip3)
 - cabextract
+- requests
 
 ## How to use
 1. Clone this repository
-2. Enter the cabs directory
-3. Run ./download.py and wait for the firmware to be downloaded
+2. Run pip3 install -r cabs/requirements.txt
+3. Run python3 cabs/download.py and wait for the firmware to be downloaded
 4. Run ./extract.sh and wait for the firmware to be extracted
-5. You'll find the extracted firmware in the firmware directory, use `cd ../firmware`
+5. You'll find the extracted firmware in the firmware directory, use `cd firmware`
 6. That's it!
 
 ## Remarks
--- cabs/download.py
+++ cabs/download.py
@@ -2,6 +2,10 @@
 import os
 import sys
 import requests
+import re
+import time
+
+from multiprocessing.pool import ThreadPool
 
 try:
 	from bs4 import BeautifulSoup as Soup
@@ -9,74 +13,91 @@ except BaseException as exc:
 	print("You need the bs4 library, please install it with pip3!")
 	exit(1)
 
-import re
-s = requests.Session()
-links = Soup(s.get("https://sites.google.com/site/lsw2uy2kjx/navcore_tomtom").text, "html.parser").find_all("a")
 
-print("Downloading {} TomTom firmwares...".format(len(links)))
-for link in links:
-	if link.text.endswith(".cab"):
-		file = link.text.rsplit('/',1)[-1]
-		if os.path.exists(file):
-			print("Skipping {}, already downloaded.".format(file))
-			continue
+def download_file(file):
+	if "Google Sites" in file: # literally no idea where it is comming from, looking into it later
+		return False 
+	try:
 		print("Downloading {}...".format(file))
-		contents = s.get(link.text)
+	
+		contents = s.get(file)
 
-		if contents.status_code == 404 or "Not found" in contents.content:
-			print("Not found, skipping...")
-			continue
-		contents = contents.content
+		download_path = os.path.join("firmware", file.rsplit('/',1)[-1])
 
-		with open(file, "wb+") as f:
-			f.write(contents)
-		print("Done.")
+		if contents.status_code == requests.codes.ok:
+			with open(download_path, "wb+") as f:
+				f.write(contents.content)
+				return True
+	except Exception as exc:
+		print("Error while trying to get {}: {}".format(file ,exc))
+	return False
+
+def resolve_blogspot(): 
+	content = s.get("https://www.w3.org/services/html2txt?url=http%3A%2F%2Fsoslouz.blogspot.com%2F2018%2F08%2Fcurrent-official-navcores-for-all.html").text
+	urls = []
+
+	for i in range(0, len(content)):
+		if content[i:i+len("_http")] == "_http":
+			end = 0
+			while content[i+end:i+end+len(".cab")] != ".cab":
+				end+=1
+			link = content[i+1:i+end+len(".cab")].replace(" ", "").replace("\n", "")
+			if "_http" in link:
+				links = link.split("_http")
+				if links[0].endswith(".ca"):
+					links[0]+="b"
+				if links[0].startswith("//"):
+					links[0] = "http:"+links[0]
+				links[1] = links[1][1:]
+				if links[1].startswith("//"):
+					links[1] = "http:"+links[1]
+				if links[1].endswith(".ca"):
+					links[1]+="b"
+
+				urls.append(links[0])
+				urls.append(links[1])
+
+			else:
+				urls.append(link)
+			i+=len("_http")
+
+	return urls
+
+if __name__ == "__main__":
+	starting_time = time.time()
+	s = requests.Session()
+	links = Soup(s.get("https://sites.google.com/site/lsw2uy2kjx/navcore_tomtom").text, "html.parser").find_all("a")
+
+	cleared_urls = []
+	for link in links:
+		if link.text.endswith(".cab"):
+			file = link.text.rsplit('/',1)[-1]
+			if os.path.exists(file):
+				print("Skipping {}, already downloaded.".format(file))
+				continue
+
+			print("Added {} to downloads table.".format(file))
+			cleared_urls.append(link.text)
+
+	files_to_download = cleared_urls + resolve_blogspot()
+
+	if len(files_to_download) >= 8: # downloading 8 files at a time, don't know what value is best but to much could cause errors because system opens to many file handlers
+		thread_count = 8
+	else:
+		thread_count = len(files_to_download)
+	print("Downloading {} TomTom firmwares...".format(len(files_to_download)))
+	results = ThreadPool(thread_count ).imap_unordered(download_file, files_to_download)
+
+	bad_requests = 0
+
+	for r in results:
+		if r == False:
+			bad_requests = bad_requests + 1
+		pass
+	
+	print("Finished in {} seconds with {} bad requests.".format(str(time.time() - starting_time), bad_requests))
+	pass
 
 
-content = s.get("https://www.w3.org/services/html2txt?url=http%3A%2F%2Fsoslouz.blogspot.com%2F2018%2F08%2Fcurrent-official-navcores-for-all.html").text
 
-urls = []
 
-for i in range(0, len(content)):
-	if content[i:i+len("_http")] == "_http":
-		end = 0
-		while content[i+end:i+end+len(".cab")] != ".cab":
-			end+=1
-		link = content[i+1:i+end+len(".cab")].replace(" ", "").replace("\n", "")
-		if "_http" in link:
-			links = link.split("_http")
-			if links[0].endswith(".ca"):
-				links[0]+="b"
-			if links[0].startswith("//"):
-				links[0] = "http:"+links[0]
-			links[1] = links[1][1:]
-			if links[1].startswith("//"):
-				links[1] = "http:"+links[1]
-			if links[1].endswith(".ca"):
-				links[1]+="b"
-
-			urls.append(links[0])
-			urls.append(links[1])
-
-		else:
-			urls.append(link)
-		i+=len("_http")
-
-links = urls
-print("Downloading {} TomTom firmwares...".format(len(links)))
-for link in links:
-	if link.endswith(".cab"):
-		file = link.rsplit('/',1)[-1]
-		if os.path.exists(file):
-			print("Skipping {}, already downloaded.".format(file))
-			continue
-
-		print("Downloading {}...".format(file))
-		contents = s.get(link)
-		if contents.status_code == 404 or "Not found" in contents:
-			print("Not found, skipping...")
-			continue
-		contents = contents.content
-		with open(file, "wb+") as f:
-			f.write(contents)
-		print("Done.")
